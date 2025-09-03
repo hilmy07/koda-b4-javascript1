3381c3e4aefee0e0840442a5334c546dcd5fc4ff
Muhammad Hilmy Haidar A <85333182+hilmy07@users.noreply.github.com>
2025-09-03 12:03:17 +0700

    Add files via upload

--- index1.js
+++ index1.js
@@ -27,3 +27,28 @@ let obj = {
 };
 
 console.log(obj.str[3][1][2].man[0].the.name.of.us);
+
+const hello = {
+  world: "Hello World",
+};
+
+console.log(hello.world);
+// Hello World
+
+let my = [
+  {
+    favourite: [
+      "favourite1",
+      "favourite2",
+      "favourite3",
+      {
+        fruits: {
+          is: "Apple",
+        },
+      },
+    ],
+  },
+];
+
+console.log(my[0].favourite[3].fruits.is);
+// "Apple"
