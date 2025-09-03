44ce6d70dc51bd1997bf52efe67c30d38da602e9
Muhammad Hilmy Haidar A <85333182+hilmy07@users.noreply.github.com>
2025-09-03 16:01:30 +0700

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,20 @@
+// for (let i = 1; i <= 4; i++) {
+//   let stars = "";
+//   for (let j = 1; j <= i; j++) {
+//     stars += "* ";
+//   }
+//   console.log(stars);
+// }
+
+for (i = 0; i <= 4; i++) {
+  let stars = "";
+  for (j = 1; j <= i; j++) {
+    stars += "* ";
+  }
+  console.log(stars);
+}
+
+// *
+// * *
+// * * *
+// * * * *
